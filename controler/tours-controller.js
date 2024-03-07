df2ffe591eb9d4033946b7414e9a8791619136af
Fadul1973 <b8039457@my.shu.ac.uk>
2024-03-07 15:59:15 +0000

    Added Filtering

--- controler/tours-controller.js
+++ controler/tours-controller.js
@@ -3,16 +3,23 @@ const Tour = require('./../models/tourModel');
 // GET TOURS
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     // BUILD AND FILTTER QUERIES
-    const queryObject = { ...req.query };
+    // 1) Filtering
+    const queryObj = { ...req.query };
     const execludedFields = ['page', 'sort', 'limit', 'fields'];
-    execludedFields.forEach((el) => delete queryObject[el]);
-    console.log(req.query, queryObject);
+    execludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObject);
+    // 2) Advance filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    const query = Tour.find(JSON.parse(queryStr));
 
     // EXECUTE QUERY
     const tours = await query;
+
     res.status(200).json({
       status: 'success',
       result: tours.length,
