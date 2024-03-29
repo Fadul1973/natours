9c535ceb1aec0152273b99e501dd41888aee98bd
Fadul1973 <b8039457@my.shu.ac.uk>
2024-03-29 15:09:33 +0000

    Fixed code leak in auth controller!!

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -36,6 +37,17 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same!!',
     },
   },
+});
+
+// Encrypting password
+userSchema.pre('save', async function (next) {
+  // Only run this function if the password has changed!
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 // Creating model out of the schema
